39dc2cb5414d4c258dcf21855932d76470e999d8
huazhulin <huazhulin@meilishuo.com>
2014-01-01 23:31:29 +0800

    for user register

---
+++ App/Lib/Action/Home/UserAction.class.php
@@ -0,0 +1,161 @@
+<?php
+class UserAction extends Action {
+    public function checkLogin(){
+        $userName = $_POST['username'];
+        $passwd = $_POST['passwd'];
+        if (empty($userName) || empty($passwd)) {
+            echo "TBC";
+            return TRUE;
+        }
+        $helper = new UserProfileModel();
+        $data = $helper->getUserProfileByUserName($userName);
+        if (empty($data)) {
+            echo "TBC2";
+            return TRUE;
+        }
+        $md5 = md5($passwd);
+        if ($md5 == $data['password']) {
+            $_SESSION['user'] = $data;
+        }
+        redirect('/index.php', 1, '页面跳转中...');
+        return TRUE;
+        $this->display();
+    }
+
+    public function register() {
+        $mail = !empty($_POST['email']) ? $_POST['email'] : $_POST['email'];
+        $phone = !empty($_POST['phone']) ? $_POST['phone'] : $_POST['phone'];
+        $nickname = !empty($_POST['nickname']) ? $_POST['nickname'] : $_POST['nickname'];
+        $password = !empty($_POST['pwd']) ? $_POST['pwd'] : $_POST['pwd'];
+        if (!empty($mail)) {
+            $this->mailRegister($mail, $nickname, $password);
+        }
+        else {
+            $this->phoneRegister($phone, $password);
+        }
+        if ($result == FALSE) {
+        }
+    }
+
+    public function mailRegister($mail, $nickname, $password) {
+        if (empty($mail) || empty($password) || empty($nickname)) {
+            return TRUE;
+        }
+        $helper = new UserProfileModel();
+        $result = $helper->getUserProfileByUserName($mail, $nickname);
+        if (!empty($result)) {
+            return FALSE;
+        }
+        $activeCode = $this->sendEmail($mail);
+        $time = date("Y-m-d H:i:s");
+        $password = md5($password);
+        $cookie = md5($mail . $nickname . "huiguilin");
+        $data = array(
+                'nickname' => $nickname,
+                'email' => $mail,
+                'ctime' => $time, 
+                'password' => $password,
+                'active_code' => $activeCode,
+                'cookie' => $cookie,
+                'is_actived' => 0,
+                'invite_code' => $cookie,
+                'last_logindate' => $time,
+                'status' => 1,
+                'realname' => $nickname,
+                'phone_number' => 111111,
+                'level' => 0,
+                'isBusiness' => 0,
+                'login_times' => 1,
+                );
+        $r = $helper->addUser($data);
+        if (!empty($r)) {
+            $data['user_id'] = $r;
+            $_SESSION['user'] = $data;
+        }
+        redirect('/index.php', 1, '页面跳转中...');
+        return TRUE;
+    }
+
+    private function sendEmail($mail) {
+        $activeCode = md5($mail . ":huiguilin");
+        $content = "你好!这是来自惠桂林的账号激活邮件,请点击 http://test.huiguilin.com/Index.php/user/active?ac={$activeCode}&user={$mail}";
+        import('ORG.Email'); 
+        $data['mailto'] = $mail; 
+        $data['subject'] = '账号激活邮件'; 
+        $data['body'] = $content; 
+        $mail = new Email();
+        if ($mail->send($data)) {
+            $this->success('发送成功,请登录邮件激活');
+        } else {
+            /* $this->error('邮件发送失败...');
+             * */
+            echo '邮件发送失败<br>';
+        }
+        return $activeCode;
+    }
+
+    public function phoneRegister($phone, $password) {
+        $helper = new UserProfileModel();
+        $result = $helper->getUserProfileByPhoneNumber($phone);
+        if (!empty($result)) {
+            return FALSE;
+        }
+        $time = date("Y-m-d H:i:s");
+        $password = md5($password);
+        $mail = $phone . "#huiguilin";
+        $nickname = $phone;
+        $cookie = md5($mail . $nickname . "huiguilin");
+        $data = array(
+                'nickname' => $nickname,
+                'email' => $mail,
+                'ctime' => $time, 
+                'password' => $password,
+                'active_code' => $activeCode,
+                'cookie' => $cookie,
+                'is_actived' => 0,
+                'invite_code' => $cookie,
+                'last_logindate' => $time,
+                'status' => 1,
+                'realname' => $nickname,
+                'phone_number' => 111111,
+                'level' => 0,
+                'isBusiness' => 0,
+                'login_times' => 1,
+                );
+        $r = $helper->addUser($data);
+        if (!empty($r)) {
+            $data['user_id'] = $r;
+            $_SESSION['user'] = $data;
+        }
+        redirect('/index.php', 1, '页面跳转中...');
+    }
+
+    public function sendCheckcode() {
+        $phoneNumber = $_POST['phone_number'];
+        $code = mt_rand(0, 9) * 1000 + mt_rand(0, 9) * 100 + mt_rand(0, 9) * 10 + mt_rand(0, 9);
+        $this->send($code);
+    }
+
+    public function active() {
+        $mail = $_GET['mail'];
+        $activeCode = $_GET['ac'];
+        $checkCode = md5($mail . ":huiguilin");
+        if ($activCode != $checkCode) {
+            redirect('/index.php', 1, '页面跳转中...');
+            return FALSE;
+        }
+        $helper = new UserProfileModel();
+        $result = $helper->getUserProfileByUserName($mail, $nickname);
+        if (empty($result)) {
+            redirect('/index.php', 1, '页面跳转中...');
+            return FALSE;
+        }
+        $condition = "email = '{$mail}'";
+        $data = array(
+            'is_actived' => 1
+        );
+        $helper->updateUser($condition, $data);
+        redirect('/index.php', 1, '页面跳转中...');
+        return TRUE;
+    }
+}
--- App/Lib/Model/Home/UserProfileModel.class.php
+++ App/Lib/Model/Home/UserProfileModel.class.php
@@ -12,5 +12,42 @@ class UserProfileModel extends Model {
         return $data;
     }
 
+    public function getUserProfileByUserName($email = "", $userName = "") {
+        if (empty($userName)) {
+            return FALSE;
+        }
+        $data = $this->where("nickname = '{$userName}' 
+                                OR email = '{$email}'")->select();
+        $data = $data[0];
+        return $data;
+    }
+
+    public function getUserProfileByPhoneNumber($phone = "") {
+        if (empty($phone)) {
+            return FALSE;
+        }
+        $data = $this->where("phone_number = '{$phone}'")->select();
+        $data = $data[0];
+        return $data;
+    }
+
+
+    public function addUser($data = array()) {
+        if (empty($data)) {
+            return false;
+        }
+        $result = $this->data($data)->add();
+        return $result;
+    }
+
+    public function updateUser($condition,$data = array()) {
+        if (empty($data) ||empty($condition)) {
+            return FALSE;
+        }
+        $result = $this->where("{$condition}")->save($data);
+        return $result;
+    }
+
+
 
 }
