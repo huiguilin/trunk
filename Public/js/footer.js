d78601e05772316928f6797e4f7e749816de78f7
huiguilin <dreamstudio_2013@outlook.com>
2014-02-07 23:02:48 +0800

    20140207

--- Public/js/footer.js
+++ Public/js/footer.js
@@ -71,5 +71,55 @@ $(function(){
 		
 	});
 	//商务合作意向表单验证结束
+
+	//用户推荐合作意向表单验证
+	$('#recommend_btn_submit').click(function(event) {
+		var recommend_username =$('#recommend_username');
+		var recommend_cellphone =$('#recommend_cellphone');
+		var recommend_business =$('#recommend_business');
+		var recommend_address =$('#recommend_address');
+		var recommend_product =$('#recommend_product');
+		var recommend_content =$('#recommend_content');
+		if(recommend_username.val() ==""){
+			alert('请留下您的姓名!');
+		}else{
+			if(recommend_cellphone.val() == ""){
+				alert('请留下您的联系方式!');
+			}else{
+				if(recommend_business.val() ==""){
+					alert('请填写推荐商户名称');
+				}else{
+					if(recommend_address.val() ==""){
+						alert('请填写推荐商户的店面地址');
+					}
+					else{
+						if(recommend_product.val() ==""){
+							alert('请填写推荐特色产品的名称');
+						}
+						else{
+							if(recommend_content.val() ==""){
+								alert('请填写相关备注信息');
+							}
+							else{
+								//ajax发送请求
+								var handleURL = "/Trunk/index.php/home/Intention/handle"; //外挂js文件不认识U方法
+								$.post(handleURL, {recommend_username:recommend_username.val(),recommend_cellphone:recommend_cellphone.val(),
+									recommend_business:recommend_business.val(),recommend_address:recommend_address.val(),
+									recommend_product:recommend_product.val(),recommend_content:recommend_content.val()}, function(data, textStatus, xhr){
+								}, 'josn');
+								//ajax发送请求结束
+								$('#main #left_card p.three').css('display', 'block');
+								$('#recommend_sumbit_form').addClass('one');
+								$('#main #left_card form ul li input').val("");
+								$('#main #left_card form ul li textarea').val("");
+							}
+						}
+					}
+				}
+			}
+		}
+		
+	});
+	//用户推荐合作意向表单验证结束
 	
 })
