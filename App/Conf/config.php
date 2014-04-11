7707d06cbf61627f003696552738bc45303fa445
huazhulin <huazhulin@meilishuo.com>
2014-04-11 17:45:08 +0800

    for config

--- App/Conf/config.php
+++ App/Conf/config.php
@@ -14,9 +14,9 @@ return array(
 
     'SMTP_SERVER' => 'smtp.163.com',  // 邮件服务器
     'SMTP_PORT' => '25',  // 邮件服务器端口
-    'SMTP_USER_EMAIL' => 'linhuazhuab@163.com',  // SMTP服务器的用户邮箱(一般发件人也得用这个邮箱)
-    'SMTP_USER' => 'linhuazhuab@163.com',  // SMTP服务器账户名
-    'SMTP_PWD' => 'linhuazhU123',  // SMTP服务器账户密码
+    'SMTP_USER_EMAIL' => 'huigl@outlook.com',  // SMTP服务器的用户邮箱(一般发件人也得用这个邮箱)
+    'SMTP_USER' => 'huigl@outlook.com',  // SMTP服务器账户名
+    'SMTP_PWD' => 'abcd.1234',  // SMTP服务器账户密码
     'SMTP_MAIL_TYPE' => 'HTML',  // 发送邮件类型:HTML,TXT(注意都是大写)
     'SMTP_TIME_OUT' => 30,  // 超时时间
     'SMTP_AUTH' => true , //邮箱验证(一般都要开启),
