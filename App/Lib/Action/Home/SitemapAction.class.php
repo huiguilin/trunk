4de681fd8b612d67a9c8a40f87e2cd5ba3e61a8f
huazhulin <huazhulin@meilishuo.com>
2014-04-21 15:27:22 +0800

    for sitemap

--- App/Lib/Action/Home/SitemapAction.class.php
+++ App/Lib/Action/Home/SitemapAction.class.php
@@ -16,22 +16,32 @@ class SitemapAction extends Action {
         $partnerInfo = $partnerHelper->getSiteMapInfo();
 
         $sitemap = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\r\n";
+
         foreach($couponInfo as $k => $v){
             $url = "/index.php/coupon/detail/{$v['coupon_id']}";
+            $url = U('/Coupon', "detail={$v['coupon_id']}");
             $priority = 0.8;
             if ($v['coupon_type'] == 2) {
                 $url .= "/specialcoupon";
                 $priority = 1.0;
             }
             $sitemap .= "<url>\r\n"."<loc>". $url ."</loc>\r\n"."<priority>{$priority}</priority>\r\n<lastmod>". $v['ctime'] ."</lastmod>\r\n<changefreq>daily</changefreq>\r\n</url>\r\n";
-        }   
+        }
+        $url = U('/Coupon/specialcoupon');
+        $sitemap .= "<url>\r\n"."<loc>". $url ."</loc>\r\n"."<priority>1</priority>\r\n<lastmod>". date("Y-m-d H:i:s") ."</lastmod>\r\n<changefreq>daily</changefreq>\r\n</url>\r\n";
+        $url = U('/Coupon/coupon');
+        $sitemap .= "<url>\r\n"."<loc>". $url ."</loc>\r\n"."<priority>1</priority>\r\n<lastmod>". date("Y-m-d H:i:s") ."</lastmod>\r\n<changefreq>daily</changefreq>\r\n</url>\r\n";
+        $url = U('/Partner/partner');
+        $sitemap .= "<url>\r\n"."<loc>". $url ."</loc>\r\n"."<priority>1</priority>\r\n<lastmod>". date("Y-m-d H:i:s") ."</lastmod>\r\n<changefreq>daily</changefreq>\r\n</url>\r\n";
 
         foreach ($partnerInfo AS $k => $v) {
-
+            $url = U('/Partner/detail/', "pid={$v['partner_id']}");
+            $priority = 1.0;
+            $sitemap .= "<url>\r\n"."<loc>". $url ."</loc>\r\n"."<priority>{$priority}</priority>\r\n<lastmod>". $v['ctime'] ."</lastmod>\r\n<changefreq>weekly</changefreq>\r\n</url>\r\n";
         }
 
         $sitemap .= '</urlset>';
-        $file = fopen("/tmp/sitemapone.xml","w");
+        $file = fopen(__DIR__ . "/../../../../sitemapone.xml","w+");
         fwrite($file,$sitemap);    fclose($file);
         return TRUE;
         }
